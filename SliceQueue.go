a065e243a74fb673ca011757262c2ea3a228d03e
huxiang <1901608396@qq.com>
2022-05-23 13:11:53 +0800

    feat(safeQueue)：实现一个线程安全的队列

---
+++ SliceQueue.go
@@ -0,0 +1,38 @@
+package main
+
+import "sync"
+
+type SliceQueue struct {
+	queue []interface{}
+	mu    sync.Mutex
+}
+
+// NewSliceQueue 初始化
+func (safeQueue *SliceQueue) NewSliceQueue(n int) (q *SliceQueue) {
+	return &SliceQueue{
+		queue: make([]interface{}, 0, n),
+	}
+}
+
+// EnQueue 入队
+func (safeQueue *SliceQueue) EnQueue(v interface{}) {
+	safeQueue.mu.Lock()
+	defer safeQueue.mu.Unlock()
+	safeQueue.queue = append(safeQueue.queue, v)
+}
+
+// OutQueue 出队
+func (safeQueue *SliceQueue) OutQueue() interface{} {
+	safeQueue.mu.Lock()
+	defer safeQueue.mu.Unlock()
+	if len(safeQueue.queue) == 0 {
+		return nil
+	}
+	v := safeQueue.queue[0]
+	safeQueue.queue = safeQueue.queue[1:]
+	return v
+}
+
+func main() {
+
+}
